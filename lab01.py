69e4b58f6d21dfd3aa3c3c020cb4095f507c2505
Steve Helms <steve@cadenrock.com>
2020-01-26 21:37:45 -0600

    Create lab01.py skeleton

---
+++ lab01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+Like the title suggests, this project involves writing a program that simulates rolling two dice. 
+When the program runs, it will randomly choose a number between 1 and 6 for each die. The program 
+will print what that numbers are. It should then ask you if you’d like to roll again. For this 
+project, you’ll need to set the min and max number that your dice can produce. For the average die, 
+that means a minimum of 1 and a maximum of 6. You’ll also want a function that randomly grabs 
+numbers within that range and prints them.
+
+Concepts to keep in mind:
+•	Random
+•	Integer
+•	Print
+•	While Loops
+
+The program must be written in Python 3.
+
+'''
